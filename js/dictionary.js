af908b180b4192c272e3c9d54eb2f16e5a493501
Jeong-minji <alswl4152@naver.com>
2018-06-08 15:13:28 +0900

    first

---
+++ js/dictionary.js
@@ -0,0 +1,7 @@
+$('#btn_next').click(function() {
+    $('#scroll_area').animate({ scrollLeft: '+=1405'}, 1000);
+});
+
+$('#btn_prev').click(function() {
+    $('#scroll_area').animate({ scrollLeft: '-=1405'}, 1000);
+});
